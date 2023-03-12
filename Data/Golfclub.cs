4228c023cd35dfa5c72a88d6f906ab5d3671f981
bloobsky <bloobsky@gmail.com>
2023-03-12 17:50:43 +0000

    Tees added.

--- Data/Golfclub.cs
+++ Data/Golfclub.cs
@@ -106,4 +106,17 @@ namespace GolfClubApp.Data
             return booking;
         }
     }
+    public static class TeeBookingNamesToString
+    {
+        public static string NamesToString(this TeeBooking booking)
+        {
+            string result = booking.Golfers.Any() == true ? booking.Golfers.First().ToString() : string.Empty;
+            for (int i = 1; i < booking.Golfers.Count; i++)
+            {
+                result = result + "," + booking.Golfers[i].ToString();
+            }
+
+            return result;
+        }
+    }
 }
